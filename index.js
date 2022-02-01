fc1824648c370396e75acc261c5d5ed7556bbbb0
prangya <kar.prangya123@gmail.com>
2022-02-01 19:16:08 +0530

    Commit slider

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 $('.slider-nav').slick({
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    dots: true
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+    dots: true,  
   });
